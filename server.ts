8fe8ed85362df82ec4fe18c670ebf28fe9a8196a
Macee Qi <maceeqi@Macees-MacBook-Pro-2.local>
2022-09-25 19:42:44 -0400

    implementation of files in Designing the Data Model

--- server.ts
+++ server.ts
@@ -3,12 +3,18 @@
  */
 import express, {Request, Response} from 'express';
 var cors = require('cors')
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express();  // express is a library
+app.use(cors());        // cors is tech that allows you to have people outside your domain to connect safely to your server
+app.use(express.json());        // json = format that data will be formatted as
 
-app.get('/', (req: Request, res: Response) =>
-    res.send('Welcome to Foundation of Software Engineering!'));
+
+function sayHello(req: Request, res: Response) {
+    res.send('Welcome to Foundation of Software Engineering!');
+}
+
+// Where we configure server to listen to incoming requests / messages
+// HTP Methods: get, post, put, delete
+app.get('/', sayHello);    // get takes 2 args (string, function)
 
 app.get('/hello', (req: Request, res: Response) =>
     res.send('Welcome to Foundation of Software Engineering!'));
